dba6440a83251c329f7499ba023821289e5336a8
Dave Pusey <dave@puseyuk.co.uk>
2023-10-02 14:38:37 +0100

    Initial Import

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<title>Minecraft Bingo by LORGON111</title>
+<?php /*		<link rel="icon" href="favicon.png" /> */ ?>
+		<link rel="stylesheet" href="styles.css" />
+	</head>
+	<body>
+		<h1>Minecraft Bingo by LORGON111</h1>
+		<h2>Downloads</h2>
+		<p><a href="/downloads/v3.1/world34.zip">Version 3.1 for Minecraft 1.11.2</a></p>
+		<h2>Reddit</h2>
+		<p><a href="https://www.reddit.com/r/minecraftbingo/" target="_blank">r/minecraftbingo</a></p>
+		<h2>Video</h2>
+		<p>Changes in Version 3.1</p>
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/x-udqNYXBJw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<p>Gameplay Overview (based on Version 3.0)</p>
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/3V3nDkAuMQ0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</body>
+</html>
