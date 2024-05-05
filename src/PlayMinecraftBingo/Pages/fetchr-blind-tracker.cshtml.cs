d25558474a083d442b0a942aefec29797b17759d
Dave Pusey <dave@puseyuk.co.uk>
2024-05-05 17:10:57 +0100

    Added new page "Fetchr Blind Item Tracker" (BETA)

---
+++ src/PlayMinecraftBingo/Pages/fetchr-blind-tracker.cshtml.cs
@@ -0,0 +1,42 @@
+using libFetchrActiveItems;
+using libFetchrActiveItems.DataStructures;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+
+namespace PlayMinecraftBingo.Pages
+{
+	public class fetchr_blind_trackerModel : PageModel
+    {
+		public Dictionary<string, List<ItemData>> CategoryList { get; set; } = ItemPool.GetSortedItemPool(CurrentVersions.Fetchr);
+
+		public void OnGet()
+        {
+        }
+
+		public string RenderItem(ItemData item)
+		{
+			string itemName = Translate.ItemName(item);
+
+			string invIcon = itemName switch
+			{
+				"Blue Trimmed Leather Boots" => "lapis_trim_leather_boots.png",
+				"Empty Map" => "empty_locator_map.png",
+				"Enchanted Book" => "enchanted_book.gif",
+				"Golden Apple" => "enchanted_apple.png",
+				"Magma Block" => "magma_block.gif",
+				"Milk Bucket" => "milk.png",
+				"Mossy Stone Bricks" => "infested_mossy_stone_bricks.png",
+				"Stonecutter" => "stonecutter.gif",
+				_ => itemName.Replace(" ", "_") + ".png"
+			};
+
+			bool isInMultipleCats = (item.Categories.Count > 1);
+
+			string htmlOut = "<img src=\"/images/mc-items/invicon_" + invIcon.ToLower() + "\" alt=\"" + itemName + "\" title=\"" + itemName + "\" /></td><td";
+			if (isInMultipleCats) htmlOut += " class=\"inmultiplecats\"";
+			htmlOut += ">" + itemName;
+
+			return htmlOut;
+		}
+    }
+}
---
+++ src/PlayMinecraftBingo/wwwroot/blind-tracker.js
@@ -0,0 +1,104 @@
+﻿function ResetAll()
+{
+	catStates = catStates.fill(0);
+	itemStates = itemStates.fill(0);
+
+	document.getElementById("hidecompletedcats").checked = false;
+
+	RefreshDisplay();
+}
+
+function HaveItem(i)
+{
+	itemStates[i] = 3;
+	RefreshDisplay();
+}
+
+function NotItem(i)
+{
+	itemStates[i] = 2;
+	RefreshDisplay();
+}
+
+function ClearItem(i)
+{
+	itemStates[i] = 0;
+	RefreshDisplay();
+}
+
+function RefreshDisplay()
+{
+	for (const i in itemStates)
+	{
+		if (itemStates[i] < 2) itemStates[i] = 0;
+	}
+	for (const i in catStates)
+	{
+		catStates[i] = 0;
+	}
+
+	CheckCats();
+
+	for (const i in catStates)
+	{
+		if (i > 0)
+		{
+			var itemCatId = "itemcat";
+			if (i < 100) itemCatId += "0";
+			if (i < 10) itemCatId += "0";
+			itemCatId += i;
+
+			if (document.getElementById("hidecompletedcats").checked)
+			{
+				if (catStates[i] == 1) {
+					document.getElementById(itemCatId).style.display = "none";
+				} else {
+					document.getElementById(itemCatId).style.display = "";
+				}
+			} else {
+				document.getElementById(itemCatId).style.display = "";
+			}
+		}
+	}
+
+	for (const i in itemStates)
+	{
+		if (i > 0)
+		{
+			var itemRowId = "itemrow";
+			if (i < 100) itemRowId += "0";
+			if (i < 10) itemRowId += "0";
+			itemRowId += i;
+
+			switch (itemStates[i])
+			{
+				case 1:
+					document.getElementById(itemRowId).className = "havecat";
+					document.getElementById(itemRowId + "yes").style.display = "none";
+					document.getElementById(itemRowId + "no").style.display = "none";
+					document.getElementById(itemRowId + "clear").style.display = "none";
+					break;
+				case 2:
+					document.getElementById(itemRowId).className = "notitem";
+					document.getElementById(itemRowId + "yes").style.display = "none";
+					document.getElementById(itemRowId + "no").style.display = "none";
+					document.getElementById(itemRowId + "clear").style.display = "";
+					break;
+				case 3:
+					document.getElementById(itemRowId).className = "haveitem";
+					document.getElementById(itemRowId + "yes").style.display = "none";
+					document.getElementById(itemRowId + "no").style.display = "none";
+					document.getElementById(itemRowId + "clear").style.display = "";
+					break;
+				default:
+					document.getElementById(itemRowId).className = "";
+					document.getElementById(itemRowId + "yes").style.display = "";
+					document.getElementById(itemRowId + "no").style.display = "";
+					document.getElementById(itemRowId + "clear").style.display = "none";
+					break;
+			}
+		}
+	}
+}
+
+RefreshDisplay();
